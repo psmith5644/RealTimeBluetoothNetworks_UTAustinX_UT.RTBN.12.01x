c3ec6628b6fa1e14a972b950ef850505ed3e5c9a
Patrick Smith <smithpm@usc.edu>
2024-08-06 16:53:21 -0700

    Refactor tests to use OS_Spy and FakeCortexM and FakeBSP

--- Lab2_4C123/test/test_FakeOS.c
+++ Lab2_4C123/test/test_FakeOS.c
@@ -1,11 +1,14 @@
 #include "unity.h"
-#include "FakeOS.h"
+#include "os.h"
+#include "os_Spy.h"
+#include "FakeCortexM.h"
+#include "FakeBSP.h"
 
 void test_FakeSemaphoreInit(void) {
     int32_t * semaphorePtr = malloc(sizeof(int32_t));
     OS_InitSemaphore(semaphorePtr, 2);
 
-    TEST_ASSERT_EQUAL_INT32(2, FakeOS_GetSemaphoreValue(semaphorePtr));
+    TEST_ASSERT_EQUAL_INT32(2, OS_Spy_GetSemaphoreValue(semaphorePtr));
 
     free(semaphorePtr);
 }
--- Lab2_4C123/test/test_Semaphore.c
+++ Lab2_4C123/test/test_Semaphore.c
@@ -1,10 +1,14 @@
 #include "unity.h"
-#include "FakeOS.h"
+#include "os.h"
+#include "os_Spy.h"
+#include "FakeCortexM.h"
+#include "FakeBSP.h"
+
 
 void test_SemaphoreInitValue(void) {
     int32_t * semaphorePtr = malloc(sizeof(int32_t));
     OS_InitSemaphore(semaphorePtr, 1);
-    int32_t value = FakeOS_GetSemaphoreValue(semaphorePtr);
+    int32_t value = OS_Spy_GetSemaphoreValue(semaphorePtr);
 
     TEST_ASSERT_EQUAL_INT32(1, value);
 
