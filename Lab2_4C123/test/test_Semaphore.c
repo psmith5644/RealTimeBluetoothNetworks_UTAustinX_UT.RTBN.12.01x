b86fb9ee1f7a06e332b8ec22cbe759a24cf3bfd2
Patrick Smith <smithpm@usc.edu>
2024-08-06 17:14:35 -0700

    refactor test_semaphore.c with setup and teardown

--- Lab2_4C123/test/test_Semaphore.c
+++ Lab2_4C123/test/test_Semaphore.c
@@ -4,27 +4,29 @@
 #include "FakeCortexM.h"
 #include "FakeBSP.h"
 
+int32_t * semaphorePtr;
 
-void test_SemaphoreInitValue(void) {
-    int32_t * semaphorePtr = malloc(sizeof(int32_t));
+void setUp(void) {
+    semaphorePtr = malloc(sizeof(int32_t));
     OS_InitSemaphore(semaphorePtr, 1);
-    int32_t value = OS_Spy_GetSemaphoreValue(semaphorePtr);
+}
 
-    TEST_ASSERT_EQUAL_INT32(1, value);
-
+void teardown(void) {
     free(semaphorePtr);
 }
 
+void test_SemaphoreInitValue(void) {
+    int32_t value = OS_Spy_GetSemaphoreValue(semaphorePtr);
+
+    TEST_ASSERT_EQUAL_INT32(1, value);
+}
+
 void test_SemaphoreWaitDecrement(void) {
-    int32_t * semaphorePtr = malloc(sizeof(int32_t));
-    OS_InitSemaphore(semaphorePtr, 1);
-    
     int32_t initialValue = OS_Spy_GetSemaphoreValue(semaphorePtr);
     OS_Wait(semaphorePtr);
 
     TEST_ASSERT_EQUAL_INT32(initialValue-1, OS_Spy_GetSemaphoreValue(semaphorePtr));
 
-    free(semaphorePtr);
 }
 
 // #include "mock_os.h"
