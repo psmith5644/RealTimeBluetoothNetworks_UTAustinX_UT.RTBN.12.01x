544b5c33b584c61d021e6b2f609b49102db1c92f
Patrick Smith <psmith5644@gmail.com>
2024-08-12 13:53:33 -0700

    implement SysTick_Handler to call switch threads

--- Lab2_4C123/osasm.s
+++ Lab2_4C123/osasm.s
@@ -20,6 +20,17 @@
 SysTick_Handler                ; 1) Saves R0-R3,R12,LR,PC,PSR
     CPSID   I                  ; 2) Prevent interrupt during switch
 
+    PUSH    {R4-R11}           ; 3) Push R4-R11
+    LDR     R0, =RunPt         ; 4) Load pointer to RunPt
+    LDR     R1, [R0]           ; 4.5) Load current RunPt into R1
+    STR     SP, [R1]           ; 5) Store Real SP into TCB SP
+    PUSH    {R0, LR}           ; 6) Call Scheduler
+    BL      Scheduler
+    POP     {R0, LR}
+    LDR     R1, [R0]           ; 6.5) Scheduler returns new RunPt, store into R1 
+    LDR     SP, [R1]           ; 7) SP = RunPt->SP
+    POP     {R4-R11}           ; 8) Pop R4-R11
+
     CPSIE   I                  ; 9) tasks run with interrupts enabled
     BX      LR                 ; 10) restore R0-R3,R12,LR,PC,PSR
 
